ef7dc7ebda75b3b44b7d99a69ad3189d6d9f75c7
crystalDf <chendong333@gmail.com>
2016-09-15 21:04:31 +0800

    Upgrade to API Level 24

--- app/src/main/java/com/star/recyclerviewtest/SingleFragmentActivity.java
+++ app/src/main/java/com/star/recyclerviewtest/SingleFragmentActivity.java
@@ -6,18 +6,20 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
+
     protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-        FragmentManager manager = getSupportFragmentManager();
-        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
 
         if (fragment == null) {
             fragment = createFragment();
-            manager.beginTransaction()
+            fragmentManager.beginTransaction()
                 .add(R.id.fragmentContainer, fragment)
                 .commit();
         }
